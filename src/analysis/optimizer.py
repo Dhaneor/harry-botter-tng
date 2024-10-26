3bad55236d620dc6f966e43ce6a044b3d867780e
Dhaneor <kundalini.experience@gmx.net>
2024-10-26 22:55:33 +0200

    prepare: optimizer implementation

--- src/analysis/optimizer.py
+++ src/analysis/optimizer.py
@@ -7,90 +7,12 @@ Created on July 06 21:12:20 2023
 
 @author dhaneor
 """
-import itertools
 import logging
-from typing import Sequence, Iterable, Generator, Tuple, Any
-
-# from scipy.optimize import minimize
-# from scipy.optimize import Bounds
-# from scipy.optimize import LinearConstraint
-# from scipy.optimize import NonlinearConstraint
 
 logger = logging.getLogger('main.optimizer')
 logger.setLevel(logging.DEBUG)
 
-# Type aliases
-Combinations = Generator[Tuple[Any, ...], None, None]
-
-
-def generate_param_combinations(
-    parameter_spaces: Sequence[dict[Sequence]]
-) -> list[tuple]:
-    """
-    Generate combinations of parameter values.
-
-    Parameters
-    ----------
-    parameter_spaces
-        A sequence of dictionaries representing parameter spaces.
-        Each dictionary should have keys 'mins', 'maxs', and 'steps',
-        with corresponding values as sequences of integers.
-
-    Returns
-    -------
-    List[Tuple]
-        A list of tuples representing all possible combinations of
-        parameter values from the given parameter spaces.
-    """
-    logger.debug(
-        f"generating combinations for {len(parameter_spaces)} parameter spaces"
-    )
-
-    combination_sets = []
-
-    for space in parameter_spaces:
-        for value in space.values():
-            logger.debug(f"generating combinations for {value}")
-            if len(value) == 2:
-                value.append(1)
-            mins, maxs, steps = value
-            vals = range(mins, maxs + 1, steps)
-            combination_sets.append(vals)
-
-    return list(itertools.product(*combination_sets))
-
-
-def generate_combinations(*iterables: Iterable) -> Combinations:
-    """Generates all possible combinations of elements from the given iterables.
-
-    Args:
-        *iterables: A variable number of iterable objects.
-
-    Yields:
-        A tuple representing a combination of elements, one from each iterable.
-    """
-    if not iterables:
-        yield ()
-    else:
-        first, *rest = iterables
-        for item in first:
-            for combination in generate_combinations(*rest):
-                yield (item,) + combination
-
 
 # ======================================================================================
 if __name__ == '__main__':
-    a = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
-    b = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j")
-    c = (True, False)
-    d = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
-
-    i = 0
-    for _ in generate_combinations(a, b, c, d):
-        if i == 0:
-            print("First combination:", _)
-        i += 1
-
-    print(f"Generated {i} combinations.")
-
-
+    pass
--- src/analysis/unit_tests/test_indicator.py
+++ src/analysis/unit_tests/test_indicator.py
@@ -11,14 +11,11 @@ current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 
-from indicators.indicator import (
+from indicators.indicator import (  # noqa: F401, E402
     factory,
     IIndicator,
-    Indicator,
-    FixedIndicator,
     PlotDescription,
-    get_all_indicator_names,
-)  # noqa: F401, E402
+)
 
 logger = logging.getLogger("main")
 logger.setLevel(logging.DEBUG)
@@ -29,11 +26,26 @@ logger.setLevel(logging.DEBUG)
     [
         ("SMA", {"timeperiod": 14}, "talib", "SMA"),
         ("EMA", {"timeperiod": 14}, "talib", "EMA"),
-        ("BBANDS", {"matype": 0, "timeperiod": 5, "nbdevup": 2, "nbdevdn": 2}, "talib", "BBANDS"),
+        (
+            "BBANDS",
+            {"matype": 0, "timeperiod": 5, "nbdevup": 2, "nbdevdn": 2},
+            "talib",
+            "BBANDS",
+        ),
         ("ADX", {"timeperiod": 14}, "talib", "ADX"),
-        ("MACD", {"fastperiod": 12, "slowperiod": 26, 'signalperiod': 9}, "talib", "MACD"),
-        ("rsi_overbought", {"rsi_overbought": 70, "parameter_space": {"rsi_overbought": [60, 90, 3]}}, "fixed", "rsi_overbought"),
-    ]
+        (
+            "MACD",
+            {"fastperiod": 12, "slowperiod": 26, "signalperiod": 9},
+            "talib",
+            "MACD",
+        ),
+        (
+            "rsi_overbought",
+            {"rsi_overbought": 70, "parameter_space": {"rsi_overbought": [60, 90, 3]}},
+            "fixed",
+            "rsi_overbought",
+        ),
+    ],
 )
 def test_indicator_factory(indicator_name, params, source, expected_name):
     indicator = factory(indicator_name, params=params, source=source)
@@ -52,11 +64,26 @@ def test_indicator_factory(indicator_name, params, source, expected_name):
     [
         ("SMA", {"timeperiod": 14}, "talib", "sma_14"),
         ("EMA", {"timeperiod": 14}, "talib", "ema_14"),
-        ("BBANDS", {"matype": 0, "timeperiod": 5, "nbdevup": 2, "nbdevdn": 2.2}, "talib", "bbands_5_2_2.2_0"),
+        (
+            "BBANDS",
+            {"matype": 0, "timeperiod": 5, "nbdevup": 2, "nbdevdn": 2.2},
+            "talib",
+            "bbands_5_2_2.2_0",
+        ),
         ("ADX", {"timeperiod": 14}, "talib", "adx_14"),
-        ("MACD", {"fastperiod": 12, "signalperiod": 9, "slowperiod": 26}, "talib", "macd_12_26_9"),
-        ("rsi_overbought", {"rsi_overbought": 70, "parameter_space": {"rsi_overbought": [60, 90, 3]}}, "fixed", "rsi_overbought_70"),
-    ]
+        (
+            "MACD",
+            {"fastperiod": 12, "signalperiod": 9, "slowperiod": 26},
+            "talib",
+            "macd_12_26_9",
+        ),
+        (
+            "rsi_overbought",
+            {"rsi_overbought": 70, "parameter_space": {"rsi_overbought": [60, 90, 3]}},
+            "fixed",
+            "rsi_overbought_70",
+        ),
+    ],
 )
 def test_indicator_properties(indicator_name, params, source, unique_name):
     logger.info(f"Testing {indicator_name} with params: {params} and source: {source}")
@@ -93,16 +120,48 @@ def test_indicator_properties(indicator_name, params, source, unique_name):
 @pytest.mark.parametrize(
     "indicator_name,params,source,expected_exception",
     [
-        ("SMA", {"timeperiod": 0}, "talib", ValueError),  # Example of an invalid timeperiod
-        ("SMA", {"timeperiod": 1000}, "talib", ValueError),  # Example of an invalid timeperiod
-        ("SMA", {"timeperiod": -5}, "talib", ValueError),  # Example of an invalid timeperiod
-        ("EMA", {"timeperiod": "invalid"}, "talib", TypeError),  # Example of a wrong type
-        ("BBANDS", {"matype": 0, "timeperiod": 5, "nbdevup": "invalid", "nbdevdn": 2}, "talib", TypeError),
+        (
+            "SMA",
+            {"timeperiod": 0},
+            "talib",
+            ValueError,
+        ),  # Example of an invalid timeperiod
+        (
+            "SMA",
+            {"timeperiod": 1000},
+            "talib",
+            ValueError,
+        ),  # Example of an invalid timeperiod
+        (
+            "SMA",
+            {"timeperiod": -5},
+            "talib",
+            ValueError,
+        ),  # Example of an invalid timeperiod
+        (
+            "EMA",
+            {"timeperiod": "invalid"},
+            "talib",
+            TypeError,
+        ),  # Example of a wrong type
+        (
+            "BBANDS",
+            {"matype": 0, "timeperiod": 5, "nbdevup": "invalid", "nbdevdn": 2},
+            "talib",
+            TypeError,
+        ),
         ("ADX", {"timeperiod": None}, "talib", TypeError),  # Example of a None value
-        ("MACD", {"fastperiod": 12, "slowperiod": 26, 'signalperiod': "invalid"}, "talib", TypeError),
-    ]
+        (
+            "MACD",
+            {"fastperiod": 12, "slowperiod": 26, "signalperiod": "invalid"},
+            "talib",
+            TypeError,
+        ),
+    ],
 )
-def test_indicator_factory_invalid_values(indicator_name, params, source, expected_exception):
+def test_indicator_factory_invalid_values(
+    indicator_name, params, source, expected_exception
+):
     logger.debug("Testing %s with invalid values: %s ", indicator_name, params)
     with pytest.raises(expected_exception):
         factory(indicator_name, params=params, source=source)
@@ -114,11 +173,26 @@ def test_indicator_factory_invalid_values(indicator_name, params, source, expect
     [
         ("SMA", {"timeperiod": 14}, "talib", "SMA"),
         ("EMA", {"timeperiod": 14}, "talib", "EMA"),
-        ("BBANDS", {"matype": 0, "timeperiod": 5, "nbdevup": 2, "nbdevdn": 2}, "talib", "BBANDS"),
+        (
+            "BBANDS",
+            {"matype": 0, "timeperiod": 5, "nbdevup": 2, "nbdevdn": 2},
+            "talib",
+            "BBANDS",
+        ),
         ("ADX", {"timeperiod": 14}, "talib", "ADX"),
-        ("MACD", {"fastperiod": 12, "slowperiod": 26, 'signalperiod': 9}, "talib", "MACD"),
-        ("rsi_overbought", {"rsi_overbought": 70, "parameter_space": {"rsi_overbought": [60, 90, 3]}}, "fixed", "rsi_overbought"),
-    ]
+        (
+            "MACD",
+            {"fastperiod": 12, "slowperiod": 26, "signalperiod": 9},
+            "talib",
+            "MACD",
+        ),
+        (
+            "rsi_overbought",
+            {"rsi_overbought": 70, "parameter_space": {"rsi_overbought": [60, 90, 3]}},
+            "fixed",
+            "rsi_overbought",
+        ),
+    ],
 )
 def test_parameter_combinations(indicator_name, params, source, expected_name):
     logger.debug("Testing %s with parameter combinations: %s ", indicator_name, params)
@@ -128,13 +202,5 @@ def test_parameter_combinations(indicator_name, params, source, expected_name):
     combinations = list(indicator.parameter_combinations)
     logger.info(combinations)
 
-    lst = []
-    for p in indicator.parameters:
-        i = 0
-        for _ in p:
-            i += 1
-        lst.append(i)
-
-    expected_combinations = reduce(operator.mul, lst, 1)
-
-    assert len(combinations) == expected_combinations
+    lst = [sum(1 for _ in p) for p in indicator.parameters]
+    assert len(combinations) == reduce(operator.mul, lst, 1)
