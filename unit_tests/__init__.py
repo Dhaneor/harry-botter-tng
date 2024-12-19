210b9488a789f47c0a7cf409cd1b45fb24e46556
Dhaneor <kundalini.experience@gmx.net>
2024-12-19 23:58:18 +0100

    update: rawi

--- unit_tests/__init__.py
+++ unit_tests/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+All tests in this directory are manual tests. S
+See ./tests for automated tests (pytest).
+
 Created on Thu Sep 22 13:00:23 2021
 
 @author_ dhaneor
